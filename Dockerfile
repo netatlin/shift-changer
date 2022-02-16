4ad6c4f1ad3dc62849a160129ca0cc2f94c58a77
netatlin <ruslanura@mail.ru>
2022-02-16 19:23:32 +0300

    second commit with full bot's functionality

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.9
+
+RUN pip install -U pip poetry
+
+COPY poetry.lock pyproject.toml ./
+RUN poetry export --without-hashes -f requirements.txt -o requirements.txt
+RUN pip install -r requirements.txt
+
+RUN mkdir /app
+WORKDIR /app
+COPY bot /app/bot
---
+++ bot/config.py
@@ -0,0 +1,16 @@
+import os
+
+from pathlib import PurePath
+from dotenv import load_dotenv, find_dotenv
+
+DEBUG = os.environ.get('DEBUG') or False
+ENV = os.environ.get('ENV') or 'development'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = PurePath(__file__).parent.parent
+
+load_dotenv(PROJECT_DIR.joinpath('config', '.env'))
+load_dotenv(find_dotenv(), override=True)
+
+
+TOKEN = os.environ.get('BOT_TOKEN')
